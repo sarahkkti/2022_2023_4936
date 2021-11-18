a80b7a99f8675d47c0311f0d83da416cc5a83ffa
Coach Cassie <cassie@cheerfrog.com>
2021-11-18 09:05:38 -0500

    Example programs for HW Map + Teleop Holonomic
    This commit includes two examples: One HarwareMap, and One TeleOp
    Holonomic mode.
    
    The Holonomic Mode is for Mechanum wheels. The robot will move on a
    dime. But there are two motor configurations missing. Please fix the
    configuration.
    
    The Hardware Map example is where we store and configure the robot
    parts. The Holonomic Mode reads the Hardware Map and applies the
    movement based off the configurations made there.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/HardwareMap_example.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.HardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * WAGS:  Naming convention is camel case!
+ *
+ *          front
+ *    (LF)--------(RF)
+ *    |    robot   |
+ *   (LB)--------(RB)
+ *        back
+ *
+ * Motor channel:  Left Front (LF) drive motor:        "leftFront"
+ * Motor channel:  Right Front (RF) drive motor:        "rightFront"
+ * Motor channel:  Left Back (LB) drive motor:        "leftBack"
+ * Motor channel:  Right Back (RB) drive motor:        "rightBack"
+ */
+
+public class HardwareMap_example
+{
+    /* Public OpMode members. */
+    public DcMotor  leftFront   = null;
+    public DcMotor  rightFront  = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareMap_example(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftFront  = hwMap.get(DcMotor.class, "leftFront");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rightFront.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_Holonomic.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_example;
+
+// name this opMode and determine a group
+@TeleOp (name="Name me", group="Which group")
+public class TeleOp_Holonomic extends OpMode {
+
+    /* Declare OpMode members. */
+    HardwareMap_example robot       = new HardwareMap_example();
+
+    @Override
+    public void init() {
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Sparks Driver!!");    //
+
+    }
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+
+        double rStickX;
+        double rStickY;
+        double lStickX;
+        double targetAngle;
+        double mag1;
+        double mag2;
+        double rotationPower;
+        double maxPower;
+        double scaleDown;
+
+        rStickX = gamepad1.right_stick_x;
+        rStickY = -gamepad1.right_stick_y;
+        lStickX = gamepad1.left_stick_x;
+
+        targetAngle = (Math.atan2(rStickY,rStickX));
+
+        rotationPower = -lStickX;
+        mag1 = Math.sqrt(Math.pow(rStickX,2) + Math.pow(rStickY,2)) * (Math.sin(targetAngle + Math.PI / 4));
+        mag2 = Math.sqrt(Math.pow(rStickX,2) + Math.pow(rStickY,2)) * (Math.sin(targetAngle - Math.PI / 4));
+
+        maxPower = Math.max(Math.abs(mag1) +  Math.abs(rotationPower) , Math.abs(mag2) +  Math.abs(rotationPower)) + 0.15;
+        scaleDown = 1.0;
+
+        if (maxPower > 1)
+            scaleDown = 1.0 / maxPower;
+
+
+        robot.leftFront.setPower((mag2 - rotationPower) * scaleDown);
+        robot.rightFront.setPower((mag1 + rotationPower) * scaleDown);
+        //robot.leftBack.setPower((mag1 - rotationPower) * scaleDown);
+        //robot.rightBack.setPower((mag2 + rotationPower) * scaleDown);
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        telemetry.addData("Say", "Good Job Team! We have STOPPED!!");
+
+    }
+
+
+}
