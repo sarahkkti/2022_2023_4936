0c7ecd120f8320ee168e735d9beccdd2304a7151
Coach Cassie <cassie@cheerfrog.com>
2022-02-10 17:46:25 -0500

    Changed lift to a motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/HardwareMap_Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap/HardwareMap_Lift.java
@@ -1,21 +1,43 @@
 package org.firstinspires.ftc.teamcode.HardwareMap;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 public class HardwareMap_Lift {
 
-    public CRServo liftServo;
-    public static final double START_INTAKE = 0;
-    //public static final double END_INTAKE   = 0.40;
+    /* Public OpMode members. */
+    public DcMotor  liftMotor   = null;
 
-    public void init (HardwareMap hardwaremap)
-    {
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
 
-        liftServo = hardwaremap.crservo.get("liftServo");
-        liftServo.setDirection(CRServo.Direction.FORWARD);
-        liftServo.setPower(0);
+    /* Constructor */
+    public HardwareMap_Lift(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        liftMotor  = hwMap.get(DcMotor.class, "duckMotor");
+
+        liftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+
+        // Set all motors to zero power
+        liftMotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //Neverest 60:1 Motor
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_Lift.java
@@ -3,40 +3,100 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_DuckMotor;
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Lift;
 import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Servo;
 
 @TeleOp(name = "liftServo Test", group = "Test")
 //@Disabled
 public class TeleOp_Lift extends OpMode {
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    HardwareMap_Servo robot       = new HardwareMap_Servo();
+    /* Declare OpMode members. */
+    HardwareMap_Lift robot       = new HardwareMap_Lift();
 
     @Override
     public void init() {
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
         robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello SPARKS Driver!!");    //
+
+    }
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
 
     }
 
     @Override
     public void loop() {
 
-        final double INCREMENT = 0.01;
-        boolean isButtonB= gamepad1.b;
+        boolean isButtonB = gamepad1.b;
         boolean isButtonA = gamepad1.a;
+        boolean isButtonX = gamepad1.x;
+        boolean isButtonY = gamepad1.y;
 
-        //double position = robot.intakeServo.getPosition();
+        double speed = 0.5;
 
         if (isButtonA) {
 
-            robot.intakeServo.setPower(1);
+            robot.liftMotor.setPower(speed);
+            robot.liftMotor.setPower(speed);
+            telemetry.addData("Button","A");
 
+            //A is retract
         } else if (isButtonB) {
 
-            robot.intakeServo.setPower(-1);
+            robot.liftMotor.setPower(-speed);
+            robot.liftMotor.setPower(-speed);
+            telemetry.addData("Button","B");
+            //B is extend
+
+        } else if (isButtonX) {
+
+            robot.liftMotor.setPower(1);
+            robot.liftMotor.setPower(1);
+            telemetry.addData("Button","X");
+            //X is retract, but with full power
+
+        }  else if (isButtonY) {
+
+            robot.liftMotor.setPower(-1);
+            robot.liftMotor.setPower(-1);
+            telemetry.addData("Button","X");
+            //X is retract, but with full power
+
+        }else {
+
+            telemetry.addData("Button","None");
+            robot.liftMotor.setPower(0);
+            robot.liftMotor.setPower(0);
 
         }
-        //telemetry.addData("Box Servo Position",String.format("%.2f", robot.intakeServo.getPosition()));
+
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        telemetry.addData("Say", "Good Job Team! We have STOPPED!!");
+
     }
 
 }
